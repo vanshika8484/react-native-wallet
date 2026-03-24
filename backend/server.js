1a28de3441ebcb723d430f8534c4d2bcedf88002
vanshika8484 <vanshikaagarwal8484@gmail.com>
2026-03-24 21:49:05 +0530

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+// const express=require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+});
+app.listen(PORT, () => {
+    console.log('Server started on port ' + PORT);
+});
